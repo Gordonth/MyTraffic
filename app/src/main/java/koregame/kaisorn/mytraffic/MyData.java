3f86915a464f22a6a043a60db984110d9f535049
gordon <thailand_club2000@yahoo.com>
2017-05-20 15:00:33 +0700

    Finish 20May

--- app/src/main/java/koregame/kaisorn/mytraffic/MyData.java
+++ app/src/main/java/koregame/kaisorn/mytraffic/MyData.java
@@ -5,4 +5,21 @@ package koregame.kaisorn.mytraffic;
  */
 
 public class MyData {
-} // Main Class
+    //Explicit
+    private String[] titleStrings = new String[]{"หัวข้อที่1","หัวข้อที่2","หัวข้อที่3","หัวข้อที่4","หัวข้อที่5",
+            "หัวข้อที่6","หัวข้อที่7","หัวข้อที่8","หัวข้อที่9","หัวข้อที่10","หัวข้อที่11","หัวข้อที่12","หัวข้อที่13",
+            "หัวข้อที่14","หัวข้อที่15","หัวข้อที่16","หัวข้อที่17","หัวข้อที่18","หัวข้อที่19","หัวข้อที่20"};
+
+    private String[] detaiStrings;
+
+    public String[] getTitleStrings() {
+        return titleStrings;
+    }
+
+    public String[] getDetaiStrings() {
+
+        String [] resultStrings;
+
+        return detaiStrings;
+    }
+} // Main Class นี่คือคลาสหลัก
